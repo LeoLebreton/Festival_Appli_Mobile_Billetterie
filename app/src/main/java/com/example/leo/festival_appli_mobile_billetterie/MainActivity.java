57e58df95b26d217d2050f0cb042633408ff36ca
LeoLebreton <llebreton1@la-joliverie.com>
2018-12-05 16:10:47 +0100

    Projet base

---
+++ app/src/main/java/com/example/leo/festival_appli_mobile_billetterie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.leo.festival_appli_mobile_billetterie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
